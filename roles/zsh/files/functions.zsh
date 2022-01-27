c2a7f34adb10c105d75ed27d11bb20f294937777
lemorse <arthurr.caron@gmail.com>
2022-01-27 10:47:35 +0100

    :sparkles: - ZSH functions updated.

--- roles/zsh/files/functions.zsh
+++ roles/zsh/files/functions.zsh
@@ -1,6 +1,14 @@
-# Descriptions: Change brightness.
+# Description: Change brightness.
 # Parameters:
 # - $1: the brightness value
 function chbrightness {
   xrandr --output eDP-1 --brightness ${1:?"The brightness value must be specified."}
+}
+
+
+# Description: Run a python http server.
+# Parameters:
+# - $1: the port (by default, 20080)
+function pysrv {
+  python3 -m http.server ${1:-20080}
 }
